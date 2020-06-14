a3a0c83f5ba843025503e00a244fdb3d73ed8694
Ombretta Strafforello <ombrettastraff@insy5.hpc.tudelft.nl>
2020-06-14 15:10:40 +0200

    commit

--- config.py
+++ config.py
@@ -43,16 +43,10 @@ class HParameters:
 
 
     def get_dataset_by_name(self, dataset_name):
-<<<<<<< HEAD
-        print(dataset_name)
-        for d in self.datasets:
 
-=======
-        
         print(dataset_name)
         for d in self.datasets:
             
->>>>>>> 724d8b7055958c2f2509e2be82bc594afeb1a4fb
             print(d)
             if dataset_name in d:
                 return [d]
