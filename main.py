fe899f8b18d5f608f4871a72531d1393a868c97a
Ombretta Strafforello <ombretta.st@gmail.com>
2020-06-14 12:51:24 +0200

    commit

--- main.py
+++ main.py
@@ -104,11 +104,14 @@ class AONet:
         """
         if datasets is None:
             datasets = self.hps.datasets
-
+        
+        print(datasets)
         if ".txt" in datasets[0]: 
             with open(datasets[0], "r") as f:
-                datasets = f.read()[0].split("\n")
-
+                datasets = f.read().split("\n")
+                if "" in datasets: datasets.remove("")
+            
+        print(datasets)
         datasets_dict = {}
         for dataset in datasets:
             print(dataset)
